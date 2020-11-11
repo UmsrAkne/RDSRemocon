42ddcfc202f684509bbe558d6b2cae1180ed5546
Akn <sibata486@gmail.com>
2020-11-11 14:30:10 +0900

    init

---
+++ RDSRemocon/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Mvvm;
+
+namespace RDSRemocon.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel() {
+
+        }
+
+        public string Output { get => output; set => SetProperty(ref output, value); }
+        private string output = "";
+    }
+}
