397c11f7745971a05c756c78211cf4faa93c61b5
Akn <sibata486@gmail.com>
2020-11-11 23:28:34 +0900

    機能追加 / RDSのインスタンス名を取得するメソッドを追加

--- RDSRemocon/ViewModels/MainWindowViewModel.cs
+++ RDSRemocon/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,22 @@
 ﻿using Prism.Mvvm;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace RDSRemocon.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+
+        private Process process = new Process() {
+            StartInfo = new ProcessStartInfo() {
+                FileName = System.Environment.GetEnvironmentVariable("ComSpec"),
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = false,
+                CreateNoWindow = true
+            }
+        };
+
         private string _title = "Prism Application";
         public string Title
         {
@@ -17,5 +30,20 @@ namespace RDSRemocon.ViewModels
 
         public string Output { get => output; set => SetProperty(ref output, value); }
         private string output = "";
+
+        /// <summary>
+        /// 現状 DBInstance は一台しか使っていないので、返却値は単一の文字列。
+        /// </summary>
+        /// <returns></returns>
+        private string getDBInstanceIdentifier (){
+            process.StartInfo.Arguments = @"/c aws rds describe-db-instances";
+            process.Start();
+
+            string text = process.StandardOutput.ReadToEnd();
+
+            var regex = new Regex("\"DBInstanceIdentifier\": \"(.*)\"", RegexOptions.IgnoreCase);
+            var matches = regex.Matches(text);
+            return matches[0].Groups[1].Value;
+        }
     }
 }
