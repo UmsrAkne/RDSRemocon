8490a84c7e80345dc731a5f070ac02649f114155
Akn <sibata486@gmail.com>
2020-11-15 20:57:42 +0900

    UI変更 / ListView の見た目を修正

---
+++ RDSRemocon/Models/Log.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDSRemocon.Models {
+    public class Log : BindableBase{
+
+        private string message;
+        private DateTime executionDateTime;
+
+        public Log(string message, DateTime executionDateTime) {
+            Message = message;
+            ExecutionDateTime = executionDateTime;
+        }
+
+        public string Message {
+            get => message; set => SetProperty(ref message, value);
+        }
+
+        public DateTime ExecutionDateTime {
+            get => executionDateTime; set => SetProperty(ref executionDateTime, value);
+        }
+    }
+}
--- RDSRemocon/ViewModels/HistoryViewModel.cs
+++ RDSRemocon/ViewModels/HistoryViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Prism.Mvvm;
+using RDSRemocon.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,5 +8,14 @@ using System.Threading.Tasks;
 
 namespace RDSRemocon.ViewModels {
     public class HistoryViewModel : BindableBase {
+
+        private List<Log> logs = new List<Log>();
+
+        public HistoryViewModel() {
+        }
+
+        public List<Log> Logs {
+            get => logs; set => SetProperty(ref logs, value);
+        }
     }
 }
