44420c1aa3b6f5f857c6490be6d625d362df8fec
Akn <sibata486@gmail.com>
2020-11-19 23:09:25 +0900

    機能追加 / タイトルバーのテキストにDBの状態文字列をバインド

--- RDSRemocon/ViewModels/MainWindowViewModel.cs
+++ RDSRemocon/ViewModels/MainWindowViewModel.cs
@@ -12,11 +12,9 @@ namespace RDSRemocon.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
         public string Title
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            get { return $"DB status : {State}"; }
         }
 
         private string state = "";
